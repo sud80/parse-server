da5fded17b58084e47c70b9569d6895bedd1ccbc
Sudaakeran Balasubramanian <sud@trimian.com>
2016-07-27 12:07:51 +0530

    test case to check beforeSave changes clobbers fetched pointer fields Basically if beforeSave makes any changes to the object it is trying to save, the fetched pointer fields on the client gets clobbered to only pointer.

--- spec/CloudCode.spec.js
+++ spec/CloudCode.spec.js
@@ -895,7 +895,7 @@ it('beforeSave should not affect fetched pointers', done => {
     });
   });
 
-  it('should fully delete objects when using `unset` with beforeSave (regression test for #1840)', done => {
+  it_exclude_dbs(['postgres'])('should fully delete objects when using `unset` with beforeSave (regression test for #1840)', done => {
     var TestObject = Parse.Object.extend('TestObject');
     var NoBeforeSaveObject = Parse.Object.extend('NoBeforeSave');
     var BeforeSaveObject = Parse.Object.extend('BeforeSaveChanged');
