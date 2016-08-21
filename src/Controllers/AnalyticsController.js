dd59ddeb9e48d9e6f23790ba7fd025ab64a6b04a
Florent Vilmart <flovilmart@users.noreply.github.com>
2016-08-21 13:57:12 -0400

    Makes sure routes don't overlap and yield a header set error (#2559)

--- src/Controllers/AnalyticsController.js
+++ src/Controllers/AnalyticsController.js
@@ -1,6 +1,8 @@
 import AdaptableController from './AdaptableController';
 import { AnalyticsAdapter } from '../Adapters/Analytics/AnalyticsAdapter';
 
+const AppOpenedEventName = 'AppOpened';
+
 export class AnalyticsController extends AdaptableController {
   appOpened(req) {
     return this.adapter.appOpened(req.body, req).then(
@@ -12,12 +14,17 @@ export class AnalyticsController extends AdaptableController {
   }
 
   trackEvent(req) {
-    return this.adapter.trackEvent(req.params.eventName, req.body, req).then(
-      function(response) {
-        return { response: response };
-      }).catch((err) => {
-        return { response: {} };
-      });
+    const eventName = req.params.eventName;
+    if (eventName === AppOpenedEventName) {
+      return this.appOpened(req);
+    }
+    return Promise.resolve().then(() => {
+      return this.adapter.trackEvent(eventName, req.body, req);
+    }).then((response) => {
+      return { response: response || {} };
+    }).catch((err) => {
+      return { response: {} };
+    });
   }
 
   expectedAdapterType() {
