7b30fd8db152fe668a9a3d3dadad665b651dbe30
Sudaakeran Balasubramanian <sud@trimian.com>
2016-08-26 22:00:39 +0530

    pass on limits if the original query has limit @shivangagarwal

--- src/RestQuery.js
+++ src/RestQuery.js
@@ -224,7 +224,8 @@ RestQuery.prototype.replaceInQuery = function() {
   }
 
   let additionalOptions = {
-    redirectClassNameForKey: inQueryValue.redirectClassNameForKey
+    redirectClassNameForKey: inQueryValue.redirectClassNameForKey,
+    limit: inQueryValue.limit,
   };
 
   var subquery = new RestQuery(
@@ -322,7 +323,8 @@ RestQuery.prototype.replaceSelect = function() {
   }
 
   let additionalOptions = {
-    redirectClassNameForKey: selectValue.query.redirectClassNameForKey
+    redirectClassNameForKey: selectValue.query.redirectClassNameForKey,
+    limit: selectValue.query.limit,
   };
 
   var subquery = new RestQuery(
